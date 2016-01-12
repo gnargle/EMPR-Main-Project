18c723d79b40d71b260d04bdf9f3c90f5f102afe
gnargle <nick.allen.1093@gmail.com>
2016-01-12 10:44:28 +0000

    inital files

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "rtc.c"
+#include "adc.c"
+
+int count = 8;
+
+int main(void){
+    serial_init();
+    rtc_init();
+    pwm_init();
+    adc_init();
+    while(1){
+        get_data_and_print();
+    }
+}
+
+void RTC_IRQHandler(void){
+    write_usb_serial_blocking("interrupt\n\r", 11);
+    PWM_MatchUpdate((LPC_PWM_TypeDef *) LPC_PWM1,2,count,PWM_MATCH_UPDATE_NOW);
+    count++;
+    if (count >=31){
+        count = 8;
+    }
+    RTC_SetTime((LPC_RTC_TypeDef *)LPC_RTC, RTC_TIMETYPE_SECOND, 0);
+    RTC_SetAlarmTime((LPC_RTC_TypeDef *) LPC_RTC, RTC_TIMETYPE_SECOND, 1);
+    RTC_ClearIntPending((LPC_RTC_TypeDef *)LPC_RTC, RTC_INT_ALARM);
+}
+
+
