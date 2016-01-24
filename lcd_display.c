e6ab0647726234b984bc2566e38a7883b74dd64b
dji501 <dji501@york.ac.uk>
2016-01-24 16:21:54 +0000

    Partially commented, parts uncommented (???)

--- lcd_display.c
+++ lcd_display.c
@@ -4,14 +4,14 @@
 #include "lcd_display.h"
 #include "lpc_types.h"
 #include "TransferCfg.h"
-#include "TransferCfg.c"
+#include "TransferCfg.c" //Includes: "types.h"
 
 #define emptychar 0xA0
 #define usedi2c LPC_I2C1
 
 void write_display(int i2c_port, uint8_t address, char char_to_send);
 uint8_t alloc_lcd_addr(uint8_t addr, int i, char* str_to_write);
-
+//(???)
 uint8_t alloc_lcd_addr(uint8_t addr, int i, char* str_to_write){
     if (addr == (0x80 + 16)){
         addr = 0x80 + 40;
@@ -30,6 +30,7 @@ void display_init(int i2c_port){
     /*while (read_busy_flag_display(i2c_port) == 1){
         // empty while just to check the flag
     }*/
+    //(???)
     sleep(10000);
     uint8_t write[11] = {0x00,0x34,0x0c,0x06,0x35,0x04,0x10,0x42,0x9f,0x34,0x02};
     I2C_M_SETUP_Type TransferCfg;
@@ -44,6 +45,7 @@ void display_init(int i2c_port){
 }
 
 void clear_display(i2c_port){
+    //Replaces all values on the lcd display with " " and resets the typing location back to start.
     uint8_t addr = 0x80;
     int i;
     for (i=0; i <33; i++){
@@ -63,6 +65,7 @@ void write_display(int i2c_port, uint8_t address, char char_to_send){
     /*while (read_busy_flag_display(i2c_port) == 1){
         write_usb_serial_blocking("busy1\n\r", 7);
     }*/
+    //Writes a char to the lcd then starts a sleep loop in order to make a delay between key presses.
     uint8_t writeadd[2] = {0x00, address};
     uint8_t writedata[2] = {0x40, char_to_send};
     sleep(1000);
@@ -80,7 +83,7 @@ void write_display(int i2c_port, uint8_t address, char char_to_send){
 }
 
 int read_busy_flag_display(int i2c_port){
-
+    //(???)
     uint8_t write[1];
     write[1] = 0x80;
     int receive[1];
@@ -98,6 +101,7 @@ int read_busy_flag_display(int i2c_port){
 
 void sleep(int loopcycles){
     int i;
+    //delay for amount of time determined by loopcycles.
     for (i = 0; i < loopcycles; i++){
     }
 }
